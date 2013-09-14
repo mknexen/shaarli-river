fe19744fee9ac5682b5170acdd609e63b1359160
mknexen <mario@myu>
2013-09-14 19:24:07 +0200

    Sat Sep 14 19:24:07 CEST 2013 - pictures

--- includes/menu.php
+++ includes/menu.php
@@ -4,6 +4,7 @@
 	<a id="link-top" class="btn btn-default" href="./top.php"><span class="glyphicon glyphicon-star"></span> Top Shared Links</a>
 	<a id="link-search" class="btn btn-default" href="./search.php"><span class="glyphicon glyphicon-search"></span> Search</a>
 	<a class="btn btn-default" target="_blank" href="https://nexen.mkdir.fr/shaarli-tv/"><span class="glyphicon glyphicon-film"></span> TV</a>
-	<a class="btn btn-default" target="_blank" href="https://shaarli.fr"><span class="glyphicon glyphicon-retweet"></span> Discussions</a>
+	<a class="btn btn-default" target="_blank" href="https://shaarlimages.net/"><span class="glyphicon glyphicon-picture"></span> Pictures</a>
+	<a class="btn btn-default" target="_blank" href="https://shaarli.fr/"><span class="glyphicon glyphicon-retweet"></span> Discussions</a>
 	<a class="btn btn-default" target="_blank" href="https://shaarli.fr/jappix/?r=shaarli@conference.dukgo.com"><span class="glyphicon glyphicon-comment"></span> Chat</a>		
 </div>
