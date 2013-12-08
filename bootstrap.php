f406573bf1c70a57c8014dfeb2a4a26d3c7f89d5
mknexen <mario@myu>
2013-12-08 19:55:20 +0100

    minor changes

--- bootstrap.php
+++ bootstrap.php
@@ -1,16 +1,12 @@
 <?php
 
-/**
- * Shaarli Flux River
- * @version 1.1 beta
- * @author: nexen (nexen@dukgo.com, nexen@irc.freenode.net#debian, https://nexen.mkdir.fr/shaarli)
- */
+$configFile = __DIR__.'/config.php';
 
-/**
- * DO NOT TOUCH AFTER THIS LINE
- */
+if( !file_exists('config.php') )
+	exit('Please setup your config.php');
+
 require_once __DIR__ . '/includes/ShaarliApiClient.php';
-require_once __DIR__ . '/config.php';
+require_once $configFile;
 
 function get_favicon_url( $feed_id ) {
 
